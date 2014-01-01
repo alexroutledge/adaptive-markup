50be7802c488eacb3ec19afba694b01d5168297b
alexroutledge <alex.routledge@gmail.com>
2014-01-01 23:48:06 +0000

    Create mobile.js

---
+++ js/mobile.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+  console.log("Small images downloaded! \n\nThis data is executing from a script loaded from a media query specifying max-width 480px! \n\nRefresh again with a larger width to see different script and images load.");
+}
