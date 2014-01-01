3c3752a2f2965ea8b2d91a05bd07308338192865
alexroutledge <alex.routledge@gmail.com>
2014-01-01 23:47:23 +0000

    Create desktop.js

---
+++ js/desktop.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+  console.log("Big images downloaded! \n\nThis data is executing from a script loaded from a media query specifying min-width 480px! \n\nRefresh again with a larger width to see different script and images load.");
+}
